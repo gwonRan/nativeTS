98a44fe779d1dc968d2d95ad09bfdce0926510fd
권미란 <gwon.rran@gmail.com>
2020-06-26 15:09:25 +0900

    typescript install

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>React Native TypeScript</Text>
-      <StatusBar style="auto" />
+      <Hello name="World" enthusiasmLevel={99} />
     </View>
   );
 }
